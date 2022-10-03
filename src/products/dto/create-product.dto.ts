5e528eb5ce1fbb2af7530de6490588bdb0051c7a
Guilherme Lucio <guilhermelucio82@gmail.com>
2022-10-03 17:07:00 -0300

    Adicionando validators do DTO

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,25 +1,71 @@
-export class CreateProductDto {
+import { Type } from "class-transformer";
+import { IsString, IsNumber, IsObject, ValidateNested, IsOptional } from "class-validator";
+
+export class Category {
+  @IsString()
+  id: string;
+  
+  @IsString()
   name: string;
+}
+
+
+export class ProductDetails {
+  @IsOptional()
+  @IsString()
+  indication?: string;
+
+  @IsString()
+  composition: string;
+
+  @IsNumber()
+  size: number;
+
+  @IsNumber()
+  weight: number;
+
+  @IsOptional()
+  @IsString()
+  color?: string;
+
+  @IsOptional()
+  @IsString()
+  flavor?:  string;
+}
+
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  pictureUrl?: string;
+
+  @IsString()
   sku: string;
+
+  @IsOptional()
+  @IsString()
   brand?: string;
+
+  @IsOptional()
+  @IsString()
   line?: string;
+
+  @IsNumber()
   price: number;
+
+  @IsString()
   description: string;
+
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Category)
   category: Category;
+
+  @IsObject()
+  @ValidateNested()
+  @Type(() => ProductDetails)
   details: ProductDetails;
-
-}
-
-export interface Category{
-    id:string;
-    name:string;
-}
-
-export interface ProductDetails{
-    indication?:String;
-    composition: String;
-    size: Number;
-    weight: Number;
-    color?: String;
-    flavor?:  String;
 }
